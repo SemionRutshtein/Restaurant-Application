6b8cf9d09d0fb8e7e5893e3213e3a7e6e3de1826
SemionRutshtein <0senja0@gmail.com>
2020-08-07 23:27:06 +0300

    Protection configuration of project Prepare to Heroku

---
+++ config/keys.dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://semion:1234qwer@cluster0-uvjtj.mongodb.net/test?retryWrites=true&w=majority',
+    jwt: 'dev-jwd'
+}
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://semion:1234qwer@cluster0-uvjtj.mongodb.net/test?retryWrites=true&w=majority',
-    jwt: 'dev-jwd'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys.prod')
+} else {
+    module.exports = require('./keys.dev')
 }
+
